ac5df14c5027d9f37020c2ebbe8a5e6cecaa77d6
Xhst <82611659+Xhst@users.noreply.github.com>
2025-12-09 20:28:25 +0100

    chore: nodiscards

--- Engine/src/core/events/window_event.hpp
+++ Engine/src/core/events/window_event.hpp
@@ -20,8 +20,8 @@ namespace pxt::core {
         WindowResizeEvent(uint32_t width, uint32_t height)
             : m_width(width), m_height(height) {}
 
-        uint32_t getWidth() const { return m_width; }
-        uint32_t getHeight() const { return m_height; }
+        [[nodiscard]] uint32_t getWidth() const { return m_width; }
+        [[nodiscard]] uint32_t getHeight() const { return m_height; }
 
         Event::Type getEventType() const override { return Event::Type::WindowResize; }
         std::string getName() const override { return "WindowResize"; }
