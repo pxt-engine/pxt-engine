b00573f61ca4940a2d15a2c62e702ada3b023ac8
Prox747 <carloproserpio1@gmail.com>
2025-12-26 18:55:01 +0100

    feat: added texture registry for editor icons etc.

---
+++ Editor/src/constants.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+const std::string EDITOR_TEXTURES_PATH = "../Editor/assets/textures/";
+
+const std::string MISSING_TEXTURE = "missing_texture.png";
+const std::string PLACEHOLDER_TEXTURE = "placeholder_texture.png";
