1a80d1d59782caf22c9c29e7332facb733f0db07
Prox747 <carloproserpio1@gmail.com>
2026-01-10 14:17:18 +0100

    refactor: renamed private members

--- Engine/src/core/input/input_state.hpp
+++ Engine/src/core/input/input_state.hpp
@@ -25,45 +25,51 @@ namespace pxt::core {
 
         void onChar(uint32_t charCode);
 
-        bool isKeyDown(KeyCode key) const { return keyDown[(size_t)key]; }
+        bool isKeyDown(KeyCode key) const { return m_keyDown[(size_t)key]; }
 
-        bool isKeyPressed(KeyCode key) const { return keyPressed[(size_t)key]; }
+        bool isKeyPressed(KeyCode key) const { return m_keyPressed[(size_t)key]; }
 
-        bool isKeyReleased(KeyCode key) const { return keyReleased[(size_t)key]; }
+        bool isKeyReleased(KeyCode key) const { return m_keyReleased[(size_t)key]; }
 
-        bool isMouseButtonDown(MouseButton button) const { return mouseDown[(size_t)button]; }
+        bool isMouseButtonDown(MouseButton button) const { return m_mouseDown[(size_t)button]; }
 
-        bool isMouseButtonPressed(MouseButton button) const { return mousePressed[(size_t)button]; }
+        bool isMouseButtonPressed(MouseButton button) const { return m_mousePressed[(size_t)button]; }
 
-        bool isMouseButtonReleased(MouseButton button) const { return mouseReleased[(size_t)button]; }
+        bool isMouseButtonReleased(MouseButton button) const { return m_mouseReleased[(size_t)button]; }
 
-        glm::vec2 getMousePosition() const { return mousePos; }
+        glm::vec2 getMousePosition() const { return m_mousePos; }
 
         glm::vec2 getMousePositionImGui() const {
             ImVec2 imguiMousePos = ImGui::GetMousePos();
             return glm::vec2(imguiMousePos.x, imguiMousePos.y);
         }
 
-        glm::vec2 getMouseDelta() const { return mouseDelta; }
+        glm::vec2 getMouseDelta() const { return m_mouseDelta; }
 
-        glm::vec2 getScrollDelta() const { return scrollDelta; }
+        glm::vec2 getScrollDelta() const { return m_scrollDelta; }
 
     private:
         // Keyboard
-        std::array<bool, (size_t)KeyCode::COUNT> keyDown{};
-        std::array<bool, (size_t)KeyCode::COUNT> keyPressed{};
-        std::array<bool, (size_t)KeyCode::COUNT> keyReleased{};
+        std::array<bool, (size_t)KeyCode::COUNT> m_keyDown{};
+        std::array<bool, (size_t)KeyCode::COUNT> m_keyPressed{};
+        std::array<bool, (size_t)KeyCode::COUNT> m_keyReleased{};
 
         // Mouse
-        std::array<bool, (size_t)MouseButton::COUNT> mouseDown{};
-        std::array<bool, (size_t)MouseButton::COUNT> mousePressed{};
-        std::array<bool, (size_t)MouseButton::COUNT> mouseReleased{};
+        std::array<bool, (size_t)MouseButton::COUNT> m_mouseDown{};
+        std::array<bool, (size_t)MouseButton::COUNT> m_mousePressed{};
+        std::array<bool, (size_t)MouseButton::COUNT> m_mouseReleased{};
 
-        glm::vec2 mousePos{0.f, 0.f};
-        glm::vec2 mouseDelta{0.f, 0.f};
-        glm::vec2 scrollDelta{0.f, 0.f};
+        glm::vec2 m_mousePos{0.f, 0.f};
+        glm::vec2 m_mouseDelta{0.f, 0.f};
+        glm::vec2 m_scrollDelta{0.f, 0.f};
 
         // Text Input
-        std::vector<uint32_t> textInput; // UTF-32 characters
+        std::vector<uint32_t> m_textInput; // UTF-32 characters
+
+        bool m_isViewportFocused = false;
+        bool m_isViewportHovered = false;
+
+        // we need this to block certain events when interacting with UI
+        bool m_isCursorOverUI = false;
     };
 } // namespace pxt::core
