6fdb5f70e79bf8e3ec38f61d718a8bb6d38a8681
Xhst <82611659+Xhst@users.noreply.github.com>
2026-01-14 22:23:06 +0100

    feat: file logger sink

---
+++ Engine/src/core/logging/file_logger_sink.cpp
@@ -0,0 +1,42 @@
+#include "core/logging/file_logger_sink.hpp"
+
+#include "core/logging/log_helper.hpp"
+
+#include <spdlog/sinks/basic_file_sink.h>
+
+namespace pxt::core {
+
+    constexpr char const* LOGGER_NAME = "PXT";
+    constexpr char const* LOG_PATH = "logs/PXT.log";
+
+    // %l for the log level (trace, debug, info, warn, error, critical)
+    // %n for the logger name
+    // %v for the log message
+    // %t for thread ID
+    // %s for the source file -> file.cpp
+    // %@ for source file and line number -> file.cpp:123
+    constexpr char const* LOG_PATTERN = "%^[%H:%M:%S] [%!] [%l]%$ %v";
+
+    FileLoggerSink::FileLoggerSink() {
+        // Create the internal spdlog file sink (true = truncate file on start)
+        auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(LOG_PATH, true);
+
+        m_internalLogger = std::make_shared<spdlog::logger>(LOGGER_NAME, sink);
+        m_internalLogger->set_pattern(LOG_PATTERN);
+    }
+
+    void FileLoggerSink::log(LogLevel level, std::string_view message, const std::source_location loc) {
+        spdlog::level::level_enum spdlogLevel = pxtToSpdlogLevel(level);
+
+        // Check if the location is valid (file_name will be empty if default initialized)
+        if (loc.file_name()[0] != '\0') {
+            std::string cleanedFunc = std::string(cleanFunctionName(loc.function_name()));
+
+            spdlog::source_loc spdLoc{loc.file_name(), static_cast<int>(loc.line()), cleanedFunc.c_str()};
+            m_internalLogger->log(spdLoc, spdlogLevel, message);
+        } else {
+            // Log without source info
+            m_internalLogger->log(spdlogLevel, message);
+        }
+    }
+} // namespace pxt::core
---
+++ Engine/src/core/logging/file_logger_sink.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "core/logging/logger_sink.hpp"
+
+// Forward declaration of spdlog classes to keep header clean
+namespace spdlog {
+    class logger;
+}
+
+namespace pxt::core {
+
+    class FileLoggerSink : public LoggerSink {
+    public:
+        FileLoggerSink();
+        virtual ~FileLoggerSink() override = default;
+
+        void log(LogLevel level, std::string_view message, const std::source_location loc = {}) override;
+
+    private:
+        Shared<spdlog::logger> m_internalLogger;
+    };
+
+} // namespace pxt::core
