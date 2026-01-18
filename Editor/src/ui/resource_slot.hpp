a3898b1d38f47c2d76d4a7e8318c155421124c8c
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2026-01-18 19:26:07 +0100

    feat: resource slot ui widget (to be finished) fro resource drag and drop

---
+++ Editor/src/ui/resource_slot.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "pxtengine.h"
+#include "ui/drag_and_drop.hpp"
+
+namespace pxt::editor {
+    class ResourceSlot {
+    public:
+        static bool render(const char* label, DragAndDrop::EnginePayload& outPayload, ResourceManager& rm) {
+            bool changed = false;
+
+            // get the name of the current asset for the button label
+            std::string buttonText = "None (Empty)";
+            if (outPayload.id != core::UUID::s_invalidId) {
+                if (Shared<Resource> resource = rm.get(outPayload.id)) {
+                    buttonText = "Drop here: " + resource->alias;
+                }
+            }
+
+            ImGui::PushID(label); // Prevent ID conflicts if using multiple slots
+          
+            // A square-ish button to act as the "Target Icon"
+            if (ImGui::Button(buttonText.c_str(), ImVec2(ImGui::GetContentRegionAvail().x, 0))) {
+                ImGui::OpenPopup("AssetSelectionPopup");
+            }
+
+            // handle Drag and Drop Target
+            if (DragAndDrop::dragDropTarget(outPayload, outPayload.type, outPayload.source)) {
+                changed = true;
+            }
+
+            // handle Manual Selection Popup
+            if (ImGui::BeginPopup("AssetSelectionPopup")) {
+                static  ImGuiTextFilter filter;
+
+                filter.Draw("Search");
+
+                //TODO: read below
+                /* this is currently not possible because some components still use pointers
+                *    and in the code we are not checking for nullptrs. (components will hold uuids or asset handles in
+                    the future)
+
+                if (ImGui::Selectable("Clear Slot")) {
+                    outPayload.id = core::UUID::s_invalidId;
+                    changed = true;
+                }
+                */
+
+                // List all resources of the specified type
+                for (auto& resource : rm.getResourcesByType(outPayload.type)) {
+                    // Filter by name and type
+                    if (filter.PassFilter(resource->alias.c_str())) {
+                        if (ImGui::Selectable(resource->alias.c_str(), outPayload.id == resource->id)) {
+                            outPayload.id = resource->id;
+                            changed = true;
+                            ImGui::CloseCurrentPopup();
+                        }
+                    }
+                }
+
+                ImGui::EndPopup();
+            }
+
+            ImGui::PopID();
+            return changed;
+        }
+    };
+}
